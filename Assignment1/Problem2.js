bf330a4b356fed5d9d2f5e45cffa053ae7b17549
Nitesh Jadhav <niteshjadhav3171@gmail.com>
2024-05-23 15:21:11 +0530

    count Repetation of no

---
+++ Assignment1/Problem2.js
@@ -0,0 +1,15 @@
+function fun(arr){
+    for(let i=0;i<arr.length;i++){
+        let count=0;
+        for(let j=0;j<arr.length;j++){
+            if(arr[i]==arr[j]){
+                count++;
+            }
+        }
+        console.log(`${arr[i]}:${count}`);
+    }
+};
+
+let arr=[1,1,2,3,4,4];
+fun(arr);
+  
