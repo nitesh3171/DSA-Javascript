9b0f2e8324a441d1083c89aa0003888768d2a80e
Nitesh Jadhav <niteshjadhav3171@gmail.com>
2024-05-29 10:31:08 +0530

    common element of array

---
+++ Assignment4/problem2.js
@@ -0,0 +1,27 @@
+// Transpose of a Matrix
+// Given a 2D array (matrix), write a program to find its transpose.
+
+function transposeMatrix(matrix) {
+    let rows = matrix.length;
+    let cols = matrix[0].length;
+    let transposedMatrix = [];
+
+    for (let i = 0; i < cols; i++) {
+        transposedMatrix[i] = [];
+        for (let j = 0; j < rows; j++) {
+            transposedMatrix[i][j] = matrix[j][i];
+        }
+    }
+
+    return transposedMatrix;
+}
+
+
+let matrix = [
+    [1, 2, 3],
+    [4, 5, 6],
+    [7, 8, 9]
+];
+
+let transposedMatrix = transposeMatrix(matrix);
+console.log(transposedMatrix);
